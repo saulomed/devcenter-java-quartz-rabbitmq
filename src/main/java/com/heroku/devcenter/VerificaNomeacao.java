dcae4094ac2e21724857d331089e46f3f238846c
Saulo Santos <saulo.correa@cast@.com.br>
2021-02-27 13:29:57 -0300

    correção de horario e tolerancia a falhas http

--- src/main/java/com/heroku/devcenter/VerificaNomeacao.java
+++ src/main/java/com/heroku/devcenter/VerificaNomeacao.java
@@ -1,59 +1,54 @@
 package com.heroku.devcenter;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
-public class VerificaNomeacao implements Runnable
-{
+public class VerificaNomeacao implements Runnable {
 
     public static final String LORENA_GRACIELY_NEVES_TABLADA = "LORENA+GRACIELY+NEVES+TABLADA";
     public static final String ALAIN = "ALAIN+ESMERALDO+LOPES";
     public static final String BRUNO = "BRUNO+DE+CASTRO+FREITAS";
     private static String textoFalha = "Nenhum resultado foi encontrado para sua pesquisa.";
     public static boolean flagEnviado = false;
+
     public void run() {
         System.out.println("Verificacao executando");
         Calendar cal = Calendar.getInstance();
-//        cal.set(Calendar.DAY_OF_MONTH,11);
-        cal.set(Calendar.HOUR_OF_DAY,15);
-        cal.set(Calendar.MINUTE,30);
+        // cal.set(Calendar.DAY_OF_MONTH,11);
+        cal.set(Calendar.HOUR_OF_DAY, 20);
+        cal.set(Calendar.MINUTE, 30);
 
         Date now = new Date();
-        System.out.println("Agora: "+now);
-        System.out.println("Hora Disparo: "+cal.getTime());
-        System.out.println("Data de verificacao: "+getCurrentDay());
-        System.out.println("flag: "+flagEnviado);
+        System.out.println("Agora: " + now);
+        System.out.println("Hora Disparo: " + cal.getTime());
+        System.out.println("Data de verificacao: " + getCurrentDay());
+        System.out.println("flag: " + flagEnviado);
 
-        if(!flagEnviado && now.after(cal.getTime()))
-        {
+        if (!flagEnviado && now.after(cal.getTime())) {
             System.out.println("Realizar verificação");
             verificaNomeacao(LORENA_GRACIELY_NEVES_TABLADA, "Lorena");
             verificaNomeacao(ALAIN, "Alain");
             verificaNomeacao(BRUNO, "Bruno");
             flagEnviado = true;
             System.out.println("Verificação realizada com sucesso");
-        }
-        else if(flagEnviado && now.before(cal.getTime()))
-        {
+        } else if (flagEnviado && now.before(cal.getTime())) {
             System.out.println("Marcar flag false");
             flagEnviado = false;
-        }
-        else
-        {
+        } else {
 
             System.out.println("Verificação já realizada ou flag já alterada. Nada a ser realizado");
         }
 
-        System.out.println("Flag: "+flagEnviado);
-
-
-
+        System.out.println("Flag: " + flagEnviado);
 
     }
 
@@ -61,24 +56,34 @@ public class VerificaNomeacao implements Runnable
         URL url = null;
         String codigoPagina = null;
         try {
-//            url = new URL("https://doem.org.br/pe/petrolina/pesquisar?keyword=LORENA+GRACIELY+NEVES+TABLADA&data_publicacao=2021-02-07");
+            // url = new
+            // URL("https://doem.org.br/pe/petrolina/pesquisar?keyword=LORENA+GRACIELY+NEVES+TABLADA&data_publicacao=2021-02-07");
             String endereco = "http://doem.org.br/pe/petrolina/pesquisar?keyword=%s&data_publicacao=%s";
-//            String endereco = "http://doem.org.br/pe/petrolina/pesquisar?keyword=LORENA+GRACIELY+NEVES+TABLADA&data_publicacao=2021-02-09";
-            endereco = String.format(endereco,nomeBusca,getCurrentDay());
+            // String endereco =
+            // "http://doem.org.br/pe/petrolina/pesquisar?keyword=LORENA+GRACIELY+NEVES+TABLADA&data_publicacao=2021-02-09";
+            endereco = String.format(endereco, nomeBusca, getCurrentDay());
             System.out.println(endereco);
-            url = new URL(endereco);
-            System.setProperty("http.agent", "Chrome");
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
+            // url = new URL(endereco);
+            // System.setProperty("http.agent", "Chrome");
+            // HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            // connection.setRequestMethod("GET");
+            // connection.connect();
 
+            HttpURLConnection connection = realizaConsulta(endereco);
             int code = connection.getResponseCode();
-            System.out.println("Response code of the object is "+code);
-            if (code==200)
-            {
+            System.out.println("Response code of the object is " + code);
+            // int code = connection.getResponseCode();
+
+            if (code != 200) {
+
+                endereco = endereco.replace("http", "https");
+                connection = realizaConsulta(endereco)
+
+            }
+            
+            if (code == 200) {
                 System.out.println("OK");
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(connection.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String inputLine;
                 StringBuffer response = new StringBuffer();
 
@@ -87,36 +92,45 @@ public class VerificaNomeacao implements Runnable
                 }
                 in.close();
 
-                //print result
+                // print result
                 codigoPagina = response.toString();
-//                System.out.println("Pagina Resposta: "+codigoPagina);
+                // System.out.println("Pagina Resposta: "+codigoPagina);
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-
         Mail email = new Mail();
-        nomeEmail = nomeEmail + " - "+getCurrentDay();
-        if(codigoPagina != null && codigoPagina.contains(textoFalha))
-        {
-            email.enviaEmail(textoFalha,nomeEmail);
-        }
-        else if(codigoPagina != null && !codigoPagina.contains(textoFalha))
-        {
-            email.enviaEmail("Verifiquei o diario, possivel nomeação",nomeEmail);
+        nomeEmail = nomeEmail + " - " + getCurrentDay();
+        if (codigoPagina != null && codigoPagina.contains(textoFalha)) {
+            email.enviaEmail(textoFalha, nomeEmail);
+        } else if (codigoPagina != null && !codigoPagina.contains(textoFalha)) {
+            email.enviaEmail("Verifiquei o diario, possivel nomeação", nomeEmail);
+        } else if (codigoPagina == null) {
+            email.enviaEmail("Falha ao consultar nomeação", nomeEmail);
         }
 
         System.out.println("fim Verificacao");
     }
 
-    private String getCurrentDay(){
-        //Create a Calendar Object
+    private HttpURLConnection realizaConsulta(String endereco) throws IOException {
+        URL url = null;
+        url = new URL(endereco);
+        System.setProperty("http.agent", "Chrome");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+
+        return connection;
+    }
+
+    private String getCurrentDay() {
+        // Create a Calendar Object
         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
         String padraoData = "YYYY-MM-dd";
 
-
-        //Integer to String Conversion
+        // Integer to String Conversion
         String todayStr = new SimpleDateFormat(padraoData).format(calendar.getTime());
         System.out.println("Today Str: " + todayStr + "\n");
 
